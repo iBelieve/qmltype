b00d1749b6d4889c84159622fd41572b9dffbb9c
Michael Spencer <sonrisesoftware@gmail.com>
2015-09-09 09:27:55 -0500

    refactor: Remove unrelated source files

--- assembla.ts
+++
@@ -1,9 +0,0 @@
-/// QML .pragma library
-/// QML .import "http.js" as Http
-/// QML .import "promises.js" as Promises
-import * as Http from "http"
-import * as Promises from "promises"
-
-function getSpaceNames(): string[] {
-	
-}
